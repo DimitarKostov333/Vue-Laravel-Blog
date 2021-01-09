201c0cb8081f32b3675c85502afb816a662a11ab
DimitarKostov333 <dim2517@gmail.com>
2021-01-09 10:07:20 +0200

    JS file cleaned up.
    Signed-off-by: DimitarKostov333 <dim2517@gmail.com>

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,6 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
-let axios = require('axios');
 
 /**
  * The following block of code may be used to automatically register your
@@ -20,28 +19,12 @@ let axios = require('axios');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('posts', require('./components/posts.vue').default);
+//Vue.component('posts', require('./components/posts.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
-// const alert = new Vue({
-//     el: '#loggedInAlert',
-//     data() {
-//         return {
-//             elementVisible: true
-//         }
-//     },
-//     mounted () {
-//         setTimeout(() => this.elementVisible = false, 6000, this)
-//     }
-// });
-
-const posts = new Vue({
-    el: '#posts'
-});
 
 

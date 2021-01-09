fe66fbeb2c334c0e32bd4f8e435a2de2ff9d54ee
DimitarKostov333 <dim2517@gmail.com>
2021-01-09 10:11:30 +0200

    Cleaned up html on main post page.
    Signed-off-by: DimitarKostov333 <dim2517@gmail.com>

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,7 +14,7 @@
                 @endif
                 @if(Session::has('error'))
                     <div class="alert alert-danger">
-                        {{ Session::get("success") }}
+                        {{ Session::get("error") }}
                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
